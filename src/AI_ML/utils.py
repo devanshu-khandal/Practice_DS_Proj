734001d348ed2517025fd3c8e1c52d76c0e871c1
Devanshu Khandal <devanshu.khandal.27@gmail.com>
2025-03-09 13:41:28 +0530

    model training

--- src/AI_ML/utils.py
+++ src/AI_ML/utils.py
@@ -8,6 +8,9 @@ import pymysql
 from dotenv import load_dotenv
 import pickle
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
 load_dotenv()
 
 host = os.getenv("host")
@@ -18,7 +21,6 @@ port = os.getenv("port")
 # print(str(database))
 def read_sql_data():
     # logging.info("Reading data from MySQL database")
-
     try:
         conn = pymysql.connect(
             host=host,
@@ -36,7 +38,6 @@ def read_sql_data():
         logging.info("Data read successfully")
 
         return df
-    
     except Exception as ex:
         logging.error("Error in connecting to MySQL database")
         raise CustomException(ex,sys)
@@ -45,12 +46,42 @@ def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
-        
         with open(file_path, 'wb') as file:
             pickle.dump(obj, file)
             logging.info(f"Object saved at {file_path}")
-
     except Exception as ex:
-        
         logging.error("Error in saving object")
         raise CustomException(ex, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            logging.info(f"Best Params are {gs.best_params_}")
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except Exception as e:
+        logging.error("Error in Evaluation of model")
+        raise CustomException(e,sys)
